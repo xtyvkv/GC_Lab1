34917ac55a51ccc7bff33c514ecc41c891d4574c
Christy Hartner <xtyvkv@gmail.com>
2022-04-16 23:43:33 -0400

    github test

---
+++ GC_Lab1/GC_Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GC_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Number Analyzer!");
+        }
+    }
+}
