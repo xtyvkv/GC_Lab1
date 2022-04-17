164cb12d298eb9ee263812023ab5afd9e78adace
Christy Hartner <xtyvkv@gmail.com>
2022-04-17 00:51:06 -0400

    initial logic build progress

--- GC_Lab1/GC_Lab1/Program.cs
+++ GC_Lab1/GC_Lab1/Program.cs
@@ -6,7 +6,31 @@ namespace GC_Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Number Analyzer!");
+            Console.WriteLine("Please enter your name.");
+            string name = Console.ReadLine();
+            
+            var inputInt = 0;
+
+            Console.WriteLine("Hello, " + name + "! Please enter an integer between 1 and 100.");
+            var input = Console.ReadLine();
+
+            while (!int.TryParse(input, out inputInt) || inputInt <= 0 || inputInt > 100)
+            {
+                Console.WriteLine("Incorrect input, " + name + ".");
+                Console.WriteLine("Please enter an integer between 1 and 100.");
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Thank you, " + name + ". " + $"You said {inputInt}!");
+
+            if (inputInt%2 == 0)
+            {
+                Console.WriteLine("Even.");
+            }
+            else
+            {
+                Console.WriteLine("Odd.");        
+            }
         }
     }
 }
