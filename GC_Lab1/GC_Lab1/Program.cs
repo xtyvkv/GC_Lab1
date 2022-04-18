482034fad00cc85ef3015e8291f190fd9a85d447
Christy Hartner <xtyvkv@gmail.com>
2022-04-18 01:16:36 -0400

    added build specifications

--- GC_Lab1/GC_Lab1/Program.cs
+++ GC_Lab1/GC_Lab1/Program.cs
@@ -14,22 +14,49 @@ namespace GC_Lab1
             Console.WriteLine("Hello, " + name + "! Please enter an integer between 1 and 100.");
             var input = Console.ReadLine();
 
-            while (!int.TryParse(input, out inputInt) || inputInt <= 0 || inputInt > 100)
+            if (!int.TryParse(input, out inputInt) || inputInt <= 0 || inputInt > 100)
             {
                 Console.WriteLine("Incorrect input, " + name + ".");
                 Console.WriteLine("Please enter an integer between 1 and 100.");
-                input = Console.ReadLine();
-            }
-
-            Console.WriteLine("Thank you, " + name + ". " + $"You said {inputInt}!");
-
-            if (inputInt%2 == 0)
-            {
-                Console.WriteLine("Even.");
+                /* figure out how to restart cuz your previous idea flopped HARD */
             }
             else
             {
-                Console.WriteLine("Odd.");        
+                if (inputInt % 2 == 0)
+                {
+                    if (inputInt >= 2 && inputInt > 60)
+                    {
+                        Console.WriteLine("Thank you, " + name + ". " + $"You said {inputInt}, which is even and greater than 60.");
+                    }
+                    else
+                    {
+                        if (inputInt <= 24)
+                        {
+                            Console.WriteLine("Thank you, " + name + ". " + $"You said {inputInt}, which is even and less than 25.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Thank you, " + name + ". " + $"You said {inputInt}, which is even and between 26 and 60 inclusive.");
+                        }
+                    }
+                }
+                else
+                {
+                    if (inputInt < 60)
+                    {
+                        Console.WriteLine("Thank you, " + name + ". " + $"You said {inputInt}, which is odd and less than 60.");
+                    }
+                    else
+                    {
+                    Console.WriteLine("Thank you, " + name + ". " + $"You said {inputInt}, which is odd and greater than 60.");
+                    }
+                }
+
+                /* figure this out too. extra credit baby!!!
+                
+                Console.WriteLine("Go again?");
+
+                */
             }
         }
     }
